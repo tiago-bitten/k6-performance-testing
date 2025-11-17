4b2772e341fbc878505d9b911f3368e3c3b115bc
Tiago Silveira De Bittencourt <tiago.40123@alunosatc.edu.br>
2025-11-17 19:31:46 -0300

    metricas da prova

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -4,46 +4,53 @@ import http from 'k6/http';
 import { check } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
+export const getCatFactDuration = new Trend('get_cat_fact', true);
 export const RateContentOK = new Rate('content_OK');
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.40'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ['rate<0.25'],        
+    get_cat_fact: ['p(90)<6800'],       
+    content_OK: ['rate>0.75']          
   },
+
   stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
+    { duration: '30s', target: 7 },    
+    { duration: '90s', target: 92 },   
+    { duration: '90s', target: 92 },    
   ]
 };
 
 export function handleSummary(data) {
   return {
     './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
+    stdout: textSummary(data, { indent: ' ', enableColors: true }),
   };
 }
 
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  const baseUrl = 'https://catfact.ninja/fact';
 
   const params = {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   };
 
-  const OK = 200;
+  const res = http.get(baseUrl, params);
 
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
+  getCatFactDuration.add(res.timings.duration);
+  RateContentOK.add(res.status === 200);
 
   check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
+    'GET CatFact - Status 200': () => res.status === 200,
+    'Response has "fact" field': () => {
+      try {
+        const body = JSON.parse(res.body);
+        return body.fact !== undefined;
+      } catch {
+        return false;
+      }
+    }
   });
 }
